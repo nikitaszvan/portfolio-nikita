377ab897a2a1131eed9428ea8cb79f3318298da5
Nikita Van <nikitaszvan@gmail.com>
2023-11-28 16:04:20 -0500

    optimize loading

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,37 +1,46 @@
-import React, {useEffect} from "react"
+import React, {useState, useEffect} from "react"
 import ParticleBackground from "./ParticleBackground";
 import HeaderAnimation from "./HeaderAnimation";
+import {SquareLoader} from 'react-spinners';
  
 function App() {
-  useEffect(() => {
-    // Elements have loaded, hide the loading message
-    const loadingMessage = document.getElementById("loading-message");
-    loadingMessage.classList.add("hidden");
-  }, []);
-
-  return (
+    const [isLoading, setIsLoading] = useState(true);
+    
+    const handleLoading = () => {
+    setIsLoading(false);
+    }
+    
+    useEffect(()=>{
+    window.addEventListener("load", handleLoading);
+    return () => window.removeEventListener("load", handleLoading);
+    },[])
+    
+    return  !isLoading ? (
     <div className="App">
-      <div id="loading-message" className="hidden">Loading...</div>
-      <ParticleBackground />
-      <div className="over-background">
-        <div className="header-container">
-          <div className="left-trapezoid"></div>
-          <div className="nav-bar">
-            <a href="/">ABOUT ME</a>
-            <a href="/">PORTFOLIO</a>
-            <a href="/">WORK WITH ME</a>
-          </div>
-          <div className="nav-bar-underline"></div>
-          <div className="right-trapezoid"></div>
+    <ParticleBackground />
+    <div className="over-background">
+      <div className="header-container">
+        <div className="left-trapezoid"></div>
+        <div className="nav-bar">
+          <a href="/">ABOUT ME</a>
+          <a href="/">PORTFOLIO</a>
+          <a href="/">WORK WITH ME</a>
         </div>
-        <div className="text-over-bg">
-          <h1>Hi, I'm <span>Nikita Van</span></h1>
-          <HeaderAnimation />
-        </div>
-        <img src="images/nikita.png" alt="portrait"/>
+        <div className="nav-bar-underline"></div>
+        <div className="right-trapezoid"></div>
       </div>
-  </div>
-  );
-} 
- 
+      <div className="text-over-bg">
+        <h1>Hi, I'm <span>Nikita Van</span></h1>
+        <HeaderAnimation />
+      </div>
+      <img src="images/nikita.png" alt="portrait"/>
+    </div>
+</div>
+    )
+    :(<div><SquareLoader color="black" /></div>)
+
+  }
+    
+    
+
 export default App;
