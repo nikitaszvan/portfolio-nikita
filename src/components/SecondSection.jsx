b98c0fffb4b943efa2d089f3e052b8e6b636e991
Nikita Van <nikitaszvan@gmail.com>
2023-11-29 21:08:46 -0500

    adjust banner

--- src/components/SecondSection.jsx
+++ src/components/SecondSection.jsx
@@ -4,11 +4,24 @@ import Hexagon from './Hexagon.jsx';
 const SecondSection = () => {
   return (
     <div className="second-section">
-      <div className="hexagon-container">
+      <div className="banner">
+        <div className="parallelogram-container">
+        <div className="left-parallelogram_1"></div>
+        <div className="left-parallelogram_2"></div>
+        <div className="left-parallelogram_3"></div>
+        <div className="left-parallelogram_4"></div>
+        <div className="right-parallelogram_1"></div>
+        <div className="right-parallelogram_2"></div>
+        <div className="right-parallelogram_3"></div>
+        <div className="right-parallelogram_4"></div>
+        </div>
+      {/* </div> */}
+      {/* <div className="hexagon-container">
         <Hexagon />
         <Hexagon />
         <Hexagon />
-    </div>
+    </div> */}
+  </div>
   </div>
   )
 }
