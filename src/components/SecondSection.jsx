51ef95fe1a31f3ca4229dafdd22bbf0593325a3f
Nikita Van <nikitaszvan@gmail.com>
2023-12-01 10:50:33 -0500

    css effects

--- src/components/SecondSection.jsx
+++ src/components/SecondSection.jsx
@@ -7,30 +7,31 @@ const SecondSection = () => {
 
  <div className="hexagon-container">
         <Hexagon 
-        hexagon_top_bp_color={{borderBottomColor: '#535F6E'}}
-        hexagon_bottom_bp_color={{borderTopColor: '#72636C'}}
+        hexagon_top_bp_color={{borderBottomColor: '#9b6469'}}
+        hexagon_bottom_bp_color={{borderTopColor: '#6C7981'}}
         hexagon_top_bg_color={{borderBottomColor: '#0E1425'}}
         hexagon_bottom_bg_color={{borderTopColor: '#0E1425'}}
-        hexagon_top_color={{borderBottomColor: '#D5D3D0'}}
-        hexagon_bottom_color={{borderTopColor: '#D5D3D0'}}
+        hexagon_top_color={{borderBottomColor: '#eedece'}}
+        hexagon_bottom_color={{borderTopColor: '#eedece'}}
         />
         <Hexagon
-        hexagon_top_bp_color={{borderBottomColor: '#C59A98'}}
-        hexagon_bottom_bp_color={{borderTopColor: '#C59A98'}}
+        hexagon_top_bp_color={{borderBottomColor: '#C69C9C'}}
+        hexagon_bottom_bp_color={{borderTopColor: '#C69C9C'}}
         hexagon_top_bg_color={{borderBottomColor: '#0E1425'}}
         hexagon_bottom_bg_color={{borderTopColor: '#0E1425'}}
-        hexagon_top_color={{borderBottomColor: '#D5D3D0'}}
-        hexagon_bottom_color={{borderTopColor: '#D5D3D0'}}
+        hexagon_top_color={{borderBottomColor: '#eedece'}}
+        hexagon_bottom_color={{borderTopColor: '#eedece'}}
         />
         <Hexagon
         hexagon_top_bp_color={{borderBottomColor: '#9D5359'}}
         hexagon_bottom_bp_color={{borderTopColor: '#9D5359'}}
-        hexagon_top_bg_color={{borderBottomColor: '#0E1425'}}
-        hexagon_bottom_bg_color={{borderTopColor: '#0E1425'}}
-        hexagon_top_color={{borderBottomColor: '#D5D3D0'}}
-        hexagon_bottom_color={{borderTopColor: '#D5D3D0'}}
+        hexagon_top_bg_color={{borderBottomColor: '#9D5359'}}
+        hexagon_bottom_bg_color={{borderTopColor: '#9D5359'}}
+        hexagon_top_color={{borderBottomColor: '#eedece'}}
+        hexagon_bottom_color={{borderTopColor: '#eedece'}}
         />
     </div>
+    <div className="hexagon-gradient"></div>
   </div>
 
   )
